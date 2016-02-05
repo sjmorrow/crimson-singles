c58bbb9a7f8ab8eceb60dbbdde2d7e51b151abab
Stephen Morrow <smorrow@credera.com>
2016-02-04 22:52:21 -0600

    Initial Commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('crimsonSingles', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngMessages', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'firebase']);
+
+})();
