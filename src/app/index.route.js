7ebecf5daefc8396cc90baace7a80a8863f3b8a9
Stephen Morrow <smorrow@credera.com>
2016-02-05 20:42:43 -0600

    Added routing and improved navigation bar with slide out menu

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,21 +1,33 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('crimsonSingles')
-    .config(routerConfig);
+    angular
+        .module('crimsonSingles')
+        .config(routerConfig);
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      });
+    /** @ngInject */
+    function routerConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('login', {
+                url: '/',
+                templateUrl: 'app/login/login.html',
+                controller: 'MainController',
+                controllerAs: 'main'
+            })
+            .state('home', {
+                url: '/home',
+                templateUrl: 'app/main/main.html',
+                controller: 'MainController',
+                controllerAs: 'main'
+            })
+            .state('home.cards', {
+                url: '/cards',
+                templateUrl: 'app/cards/cards.html',
+                controller: 'MainController',
+                controllerAs: 'main'
+            });
 
-    $urlRouterProvider.otherwise('/');
-  }
+        $urlRouterProvider.otherwise('/');
+    }
 
 })();
