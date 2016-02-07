25430c593d9a9bc5f91179ee37fbdcffe47e1626
Stephen Morrow <smorrow@credera.com>
2016-02-07 14:26:32 -0600

    Working "My Guardians" page and some styling updates

--- src/app/guardians/guardians.controller.js
+++ src/app/guardians/guardians.controller.js
@@ -10,6 +10,9 @@
         var vm = this;
         var fbRef = new Firebase(FIREBASE_URL + '/users/' + currentAuth.uid + '/guardians');
         vm.guardians = $firebaseArray(fbRef);
+        vm.updateGuardian = function(key) {
+            vm.guardians.$save(key);
+        };
         vm.addGuardian = function() {
             vm.guardians.$add({
                 preferred_subclass: '',
@@ -18,6 +21,14 @@
                 class: '',
                 bio: ''
             });
+        };
+        vm.selectClass = function(key, clazz) {
+            vm.guardians[key].class = clazz;
+            vm.guardians.$save(key);
+        };
+        vm.selectPlatform = function(key, platform) {
+            vm.guardians[key].platform = platform;
+            vm.guardians.$save(key);
         };
     }
 })();
