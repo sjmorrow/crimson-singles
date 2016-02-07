fc5ab4f1be363b3815c532845269772a6e796603
Sam Wilson <swilson@credera.com>
2016-02-07 10:00:29 -0600

    Manally added angular swing usage. Updated height of cards to fit maximum amount of display information

--- src/app/cards/cards.controller.js
+++ src/app/cards/cards.controller.js
@@ -9,6 +9,16 @@
     function CardsController(Firebase, $firebaseObject, $log) {
         var vm = this;
         var fbRef = new Firebase("https://blinding-heat-3514.firebaseio.com");
+        vm.swingOptions = {
+            throwOutConfidence: function (offset, element) {
+                $log.debug('confidence');
+                return Math.min(2 * Math.abs(offset) / element.offsetWidth, 1);
+            },
+            isThrowOut: function (offset, element, throwOutConfidence) {
+                $log.debug('isThrowOut', offset, element.offsetWidth, throwOutConfidence);
+                return throwOutConfidence === 1;
+            }
+        }
         vm.firebase = $firebaseObject(fbRef);
         vm.refresh = function() {
             $log.debug('refreshed');
@@ -18,21 +28,36 @@
             if (eventObject && eventObject.target) {
                 target = eventObject.target;
             } else {
-                target = $('[swing-card]:visible').last();
+                target = angular.element('[swing-card]:visible').last();
             }
-            $(target).fadeOut();
+            angular.element(target).fadeOut();
         }
         vm.swipeRight = function(eventObject) {
             var target;
             if (eventObject && eventObject.target) {
                 target = eventObject.target;
             } else {
-                target = $('[swing-card]:visible').last();               
+                target = angular.element('[swing-card]:visible').last(); 
+//                var card = Swing.Stack().getCard(target)
+//                if (card) {
+//                    card.throwOut(Card.DIRECTION_RIGHT, 0);
+//                }
             }
-            $(target).fadeOut();
+            angular.element(target).fadeOut();
         }
         vm.favorite = function() {
             $log.debug('favorited');
         }
+//        vm.swipe = function(eventObject) {   
+//            $log.debug(eventObject.target);
+//            if (eventObject && eventObject.throwOutConfidence > 0.5) {
+//                eventObject.target.isThrowOut = true;
+//                if (eventObject.throwDirection > 0) {
+//                    this.swipeRight(eventObject);
+//                } else {
+//                    this.swipeLeft(eventObject);
+//                }
+//            }
+//        }
     }
 })();
