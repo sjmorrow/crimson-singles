d45856483f7f44c31d2cc63fc02ad24bdd66e548
Sam Wilson <swilson@credera.com>
2016-02-07 19:09:08 -0600

    Added dom removal and pushing of liked guardian to user's matched guardian

--- src/app/cards/cards.controller.js
+++ src/app/cards/cards.controller.js
@@ -6,11 +6,11 @@
         .controller('CardsController', CardsController);
 
     /** @ngInject */
-    function CardsController(FIREBASE_URL, $firebaseObject, $firebaseArray, $log, currentAuth) {
+    function CardsController(FIREBASE_URL, $firebaseObject, 
+                              $firebaseArray, $log, currentAuth, activeGuardians) {
         var vm = this;
-        var fbRef = new Firebase("https://blinding-heat-3514.firebaseio.com");
         var matchedGuardiansRef = new Firebase(FIREBASE_URL + '/users/' + currentAuth.uid + '/matched_guardians');
-        
+        vm.activeGuardians = activeGuardians;
         vm.matchedGuardians = $firebaseArray(matchedGuardiansRef)
         vm.swingOptions = {
             throwOutConfidence: function (offset, element) {
@@ -20,47 +20,17 @@
                 return throwOutConfidence === 1;
             }
         }
-        vm.firebase = $firebaseObject(fbRef);
         vm.refresh = function() {
             $log.debug('refreshed');
         }
-        vm.swipeLeft = function(eventObject) {  
-            var target;
-            if (eventObject && eventObject.target) {
-                target = eventObject.target;
-            } else {
-                target = angular.element('[swing-card]:visible').last();
+        vm.swipeRight = function(guardian) {            
+            if (guardian) {
+                vm.matchedGuardians.$add(guardian);
             }
-            angular.element(target).fadeOut();
-        }
-        vm.swipeRight = function(guardian) {
-            $log.debug(guardian)
-//            $log.debug(angular.element('[swing-stack]'))
-//            var target;
-//            if (eventObject && eventObject.target) {
-//                target = eventObject.target;
-//            } else {
-//                target = angular.element('[swing-card]:visible').last(); 
-////                var card = Swing.Stack().getCard(target)
-////                if (card) {
-////                    card.throwOut(Card.DIRECTION_RIGHT, 0);
-////                }
-//            }
-//            angular.element(target).fadeOut();
+            vm.activeGuardians.pop();
         }
         vm.favorite = function() {
             $log.debug('favorited');
         }
-//        vm.swipe = function(eventObject) {   
-//            $log.debug(eventObject.target);
-//            if (eventObject && eventObject.throwOutConfidence > 0.5) {
-//                eventObject.target.isThrowOut = true;
-//                if (eventObject.throwDirection > 0) {
-//                    this.swipeRight(eventObject);
-//                } else {
-//                    this.swipeLeft(eventObject);
-//                }
-//            }
-//        }
     }
 })();
