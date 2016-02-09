1cda78659c8f366508c79c79b5843bf89f8a50f3
Sam Wilson <swilson@credera.com>
2016-02-08 23:49:00 -0600

    Refactored card conditional to backend controller. Added dequeue functionality

--- src/app/cards/cards.controller.js
+++ src/app/cards/cards.controller.js
@@ -18,7 +18,13 @@
         vm.matchedGuardians = $firebaseArray(matchedGuardiansRef)
         
         var favoriteGuaridansRef = new Firebase(FIREBASE_URL + '/users/' + currentAuth.uid + '/favorite_guardians');
-        vm.favoriteGuardians = $firebaseArray(favoriteGuaridansRef) 
+        vm.favoriteGuardians = $firebaseArray(favoriteGuaridansRef)
+        
+        vm.activeGuardians.$loaded().then(function(guardians) {
+            if (guardians.length) {
+                vm.activatedTimestamp = guardians[0].activatedOn
+            } 
+        });
         
         vm.swingOptions = {
             throwOutConfidence: function (offset, element) {
@@ -28,8 +34,23 @@
                 return throwOutConfidence === 1;
             }
         }
+        vm.showInStack = function(guardian) {
+            return ((vm.currentUser.XBL_ID != guardian.networkId 
+                            && guardian.platform == 'XBOX') 
+                        || (vm.currentUser.PSN_ID != guardian.networkId
+                            && guardian.platform == 'PSN'))
+                        && guardian.activatedOn >= vm.activatedTimestamp;
+        }
+        vm.dequeue = function() {
+            var removed = vm.activeGuardians.splice(0, 1)[0];            
+            if (vm.activeGuardians.length) {
+                vm.activatedTimestamp = vm.activeGuardians[0].activatedOn
+            }
+            return removed
+        }        
         vm.refresh = function() {
-            $log.debug('refreshed');
+            window.location.reload()
+            //vm.activeGuardians = $firebaseArray(activeGuardians.$ref())
         }
     }
 })();
